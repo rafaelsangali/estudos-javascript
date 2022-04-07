30df7b333f6e7b35400bb50a96a7c5e0702a3fc5
Rafael Sangali <rafaelsangali@outlook.com>
2022-04-07 15:21:37 -0300

    Execicios da aula 06/04/22 finalizados

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 //1
-
+console.log('------------------------------------ 1');
 let nomeCompleto = "Rafael Lombardi Sangali";
 
  console.log(nomeCompleto);
 
+ console.log('------------------------------------ 2');
  //2
 
  let numeroA = 5;
@@ -12,13 +13,15 @@ let nomeCompleto = "Rafael Lombardi Sangali";
 
 console.log(resultado);
 
+console.log('------------------------------------ 3');
 //3
 
-function saudacao () {
-     
+function saudacao(mensagem) {
+     return mensagem = console.log("Oi eu sou o Goku!");
 }
-console.log(saudacao("Oi eu sou o Goku!"))
+console.log(saudacao());
 
+console.log('------------------------------------ 4');
 //4
 
 function multiplica (num1, num2) {
@@ -26,27 +29,57 @@ function multiplica (num1, num2) {
 }
 console.log(multiplica(2,4))
 
+console.log('------------------------------------ 5');
+//5
+
 function podeDirigir (idade) {
     return idade >= 18 ? 'Você pode dirigir' : 'Você não pode dirigir';
 } 
 console.log(podeDirigir(18));
 
+console.log('------------------------------------ 6');
+//6
+
 for ( i = 0; i <= 20; i++) {
     console.log(i);
 }
-
-for ( i = 0; i <= 20; i++) {
-    if ( i % 2 !== 0){
-        console.log(i);
+console.log('------------------------------------ 7');
+//7
+for ( k = 0; k <= 20; k++) {
+    if ( k % 2 !== 0){
+        console.log(k);
     }
 }
 
 let resultadoTabuada;
 
-function tabuada(number) {
-    for ( j = 0; i <= 10; j++) {
-       return resultadoTabuada = number * j;
+console.log('------------------------------------ 8');
+//8
+
+function tabuada(num3) {
+    for (let index = 1; index <=10; index++) {
+        console.log(num3 + " x " + index + " = " + (num3 * index))
+      
     }
 }
+console.log(tabuada(4))
 
-console.log(tabuada(3));
+console.log('------------------------------------ 9');
+//9
+
+ function converterEmHoras (num4) {
+    if ( num4 % 60 === 0) {
+        let resultado2 = num4 / 60;
+        console.log('Isso da um total de ' + resultado2 + ' horas');
+    } if ( num4 > 60){
+        let resultado3 = parseInt(num4 / 60);
+        let resultado4 = num4 % 60;
+        console.log('Isso da um total de ' + resultado3 + ' horas e ' + resultado4 + ' minutos');
+    } if (num4 < 0){
+        console.log('Opa, opa, número negativo não vale');
+    } else if (num4 < 60){
+        console.log('Você sabe que ' + num4 + ' minutos, não são 1 hora completa');
+    }
+ }
+
+ console.log(converterEmHoras(90));
